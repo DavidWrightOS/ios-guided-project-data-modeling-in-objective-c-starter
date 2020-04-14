52036e1b34b3314deebdd898562ce6f6a48e1df6
Dimitri Bouniol <dimitri.bouniol@lambdaschool.com>
2020-04-13 19:50:36 -0700

    Finished the first unit test

--- Quakes-Objc/Quake.m
+++ Quakes-Objc/Quake.m
@@ -23,4 +23,9 @@
     return self;
 }
 
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary
+{
+    return nil;
+}
+
 @end
--- Quakes-ObjcTests/Quakes_ObjcTests.m
+++ Quakes-ObjcTests/Quakes_ObjcTests.m
@@ -9,6 +9,7 @@
 #import <XCTest/XCTest.h>
 #import "LSIFileHelper.h"
 #import "LSILog.h"
+#import "Quake.h"
 
 @interface Quakes_ObjcTests : XCTestCase
 
@@ -23,6 +24,37 @@
     
 //    NSString *quakeJSONString = [[NSString alloc] initWithData:quakeData encoding:NSUTF8StringEncoding];
 //    NSLog(@"quake (as string): %@", quakeJSONString);
+    
+    // [NSJSONSerialization JSONObjectWithData:quakeData options:NSJSONReadingMutableContainers|NSJSONReadingMutableLeaves error:NULL];
+    NSError *jsonError = nil;
+    NSDictionary *quakeDictionary = [NSJSONSerialization JSONObjectWithData:quakeData options:NSJSONReadingMutableContainers|NSJSONReadingMutableLeaves error:&jsonError]; // NSError **
+    if (!quakeDictionary) {
+        NSLog(@"We've got an error: %@", jsonError);
+    }
+    
+//    NSLog(@"quake (as a dictionary): %@", quakeDictionary);
+    
+    if (![quakeDictionary isKindOfClass:[NSDictionary class]]) {
+        NSLog(@"quakeDictionary is not a dictionary!");
+        return;
+    }
+    
+    Quake *quake = [[Quake alloc] initWithDictionary:quakeDictionary];
+    
+    NSLog(@"quake: %@", quake);
+    
+    NSDate *time = [NSDate dateWithTimeIntervalSince1970:1388620296020 / 1000.0];
+    
+    XCTAssertEqualWithAccuracy(1.29, quake.magnitude, 0.0001);
+    XCTAssertEqualObjects(@"10km SSW of Idyllwild, CA", quake.place);
+    XCTAssertEqualObjects(time, quake.time);
+    
+    // For now we'll just set the alert to nil, you may want to use @"" instead
+    XCTAssertNil(quake.alert);
+    
+    XCTAssertEqualObjects(@"earthquake", quake.type);
+    XCTAssertEqualWithAccuracy(33.663333299999998, quake.latitude, 0.0001);
+    XCTAssertEqualWithAccuracy(-116.7776667, quake.longitude, 0.0001);
 }
 
 @end
