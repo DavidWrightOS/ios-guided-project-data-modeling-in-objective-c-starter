8525d0dd2e9f1726db05f1d8c88605ec9233d9f6
Dimitri Bouniol <dimitri.bouniol@lambdaschool.com>
2020-04-13 20:21:19 -0700

    Added QuakeResults type

---
+++ Quakes-Objc/QuakeResults.h
@@ -0,0 +1,19 @@
+//
+//  QuakeResults.h
+//  Quakes-Objc
+//
+//  Created by Dimitri Bouniol Lambda on 4/13/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class Quake;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QuakeResults : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
