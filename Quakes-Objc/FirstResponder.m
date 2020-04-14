b72e94779b1085958711eeb28c825d92104f94ef
Dimitri Bouniol <dimitri.bouniol@lambdaschool.com>
2020-04-13 18:59:19 -0700

    Added a custom getter as well, which required us to specify that we need an instance variable

--- Quakes-Objc/FirstResponder.m
+++ Quakes-Objc/FirstResponder.m
@@ -8,7 +8,16 @@
 
 #import "FirstResponder.h"
 
+//@interface FirstResponder () {
+//    NSString *myInternalName;
+//}
+//
+//@end
+
 @implementation FirstResponder
+
+//@synthesize name = _name;
+@synthesize name = myInternalName;
 
 // myFirstResponder.name = @"Dimitri"
 // [myFirstResponder setName:@"Dimitri"];
@@ -16,10 +25,15 @@
 - (void)setName:(NSString *)name
 {
     // willSet
-    
-    _name = name.copy; // [name copy];
-    
+
+    myInternalName = name.copy; // [name copy];
+
     // didSet
+}
+
+- (NSString *)name
+{
+    return myInternalName.copy;
 }
 
 @end
