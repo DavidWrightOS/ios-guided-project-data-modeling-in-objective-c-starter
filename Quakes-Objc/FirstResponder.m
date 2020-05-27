6e88458f55e46a8cf723c4a037179c7425d8db14
David Wright <davidtwright89@gmail.com>
2020-05-26 20:06:15 -0400

    Added FirstResponder class

---
+++ Quakes-Objc/FirstResponder.m
@@ -0,0 +1,18 @@
+//
+//  FirstResponder.m
+//  Quakes-Objc
+//
+//  Created by David Wright on 5/25/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "FirstResponder.h"
+
+@implementation FirstResponder
+
+- (void)setName:(NSString *)name
+{
+    
+}
+
+@end
