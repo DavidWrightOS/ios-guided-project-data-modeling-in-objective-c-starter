530891d1795d92c22ab2331e16d55b9b9986d68c
David Wright <davidtwright89@gmail.com>
2020-05-26 20:31:19 -0400

    Added the interface for a Quake

---
+++ Quakes-Objc/Quake.m
@@ -0,0 +1,13 @@
+//
+//  Quake.m
+//  Quakes-Objc
+//
+//  Created by David Wright on 5/26/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "Quake.h"
+
+@implementation Quake
+
+@end
