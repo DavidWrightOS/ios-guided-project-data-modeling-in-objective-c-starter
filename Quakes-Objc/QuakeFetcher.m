f0c106e4c74ddc610b421ec9e9d98dfcfbb29116
David Wright <davidtwright89@gmail.com>
2020-05-27 22:56:18 -0400

    Simulated a default value by adding a method that has one less argument

--- Quakes-Objc/QuakeFetcher.m
+++ Quakes-Objc/QuakeFetcher.m
@@ -7,16 +7,27 @@
 //
 
 #import "QuakeFetcher.h"
+#import "NSDateInterval+DayAdditions.h"
 
 static NSString *const QuakeFetcherBaseURLString = @"https://earthquake.usgs.gov/fdsnws/event/1/query";
 
 @implementation QuakeFetcher
 
+- (void)fetchQuakesWithCompletionHandler:(nonnull QuakeFetcherCompletionHandler)completionHandler
+{
+    NSDateInterval *pastWeek = [NSDateInterval lsi_dateIntervalByAddingDays:-7];
+    [self fetchQuakesInTimeInterval:pastWeek
+                  completionHandler:completionHandler];
+}
 - (void)fetchQuakesInTimeInterval:(NSDateInterval *)interval
                 completionHandler:(QuakeFetcherCompletionHandler)completionHandler
 {
     NSURLComponents *urlComponents = [[NSURLComponents alloc] initWithString:QuakeFetcherBaseURLString];
     
+    NSISO8601DateFormatter *formatter = [[NSISO8601DateFormatter alloc] init];
+    
+    NSString *startTimeString = [formatter stringFromDate:interval.startDate];
+    NSString *entTimeString = [formatter stringFromDate:interval.endDate];
 }
 
 @end
