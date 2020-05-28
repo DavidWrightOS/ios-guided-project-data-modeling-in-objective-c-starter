c7e695838c51615d4157ff625ff2b1be985a320d
David Wright <davidtwright89@gmail.com>
2020-05-27 22:40:02 -0400

    Added a QuakeFetcher class

---
+++ Quakes-Objc/QuakeFetcher.m
@@ -0,0 +1,13 @@
+//
+//  QuakeFetcher.m
+//  Quakes-Objc
+//
+//  Created by David Wright on 5/27/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "QuakeFetcher.h"
+
+@implementation QuakeFetcher
+
+@end
