6eb4900bdd0e433398b1d4528f262a887545b15c
David Wright <davidtwright89@gmail.com>
2020-05-27 22:17:18 -0400

    Added a category on NSDateInterval named DayAdditions

---
+++ Quakes-Objc/NSDateInterval+DayAdditions.m
@@ -0,0 +1,13 @@
+//
+//  NSDateInterval+DayAdditions.m
+//  Quakes-Objc
+//
+//  Created by David Wright on 5/27/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "NSDateInterval+DayAdditions.h"
+
+@implementation NSDateInterval (DayAdditions)
+
+@end
